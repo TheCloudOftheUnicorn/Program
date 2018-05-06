bf0d91d47d00e6dcbd6d10e3c20e625b7dbe58d7
TheCloudOftheUnicorn <aliaksandra.rybakovich@mail.ru>
2018-05-06 17:45:08 +0300

    Module2 z1.part1

---
+++ Module2/Module2/LineProcessing.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Module2
+{
+    public class LineProcessing
+    {
+        private List<string> _lines;
+        private ConsoleKeyInfo keyKode;
+
+        public LineProcessing()
+        {
+            _lines = new List<string>();
+        }
+
+        public void EnterConsole()
+        {
+            while (true)
+            { 
+                keyKode = Console.ReadKey(true);
+                Console.Out.NewLine = "\n";
+                keyKode.Key != ConsoleKey.Escape ? _lines.Add(Console.ReadLine()) :  break ;
+            } 
+        }
+
+        public void GetLetters(List<string> lines)
+        {
+            foreach (var item in _lines)
+            {
+               Console.WriteLine(item[0]);
+            }
+        }
+    }
+}
---
+++ Module2/Module2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Module2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter text:\n"+"If you want stop click on Escape+Enter");
+            List<string> lineCollection = new List<string>();
+
+            LineProcessing lProc = new LineProcessing();
+
+            lProc.EnterConsole();
+            lProc.GetLetters(lineCollection);
+            Console.ReadLine();
+        }
+               
+    }
+}
