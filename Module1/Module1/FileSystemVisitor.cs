0e51602d6d6f2331bf71e62b84ec4e0013a18727
TheCloudOftheUnicorn <aliaksandra.rybakovich@mail.ru>
2018-05-06 17:35:11 +0300

    Module1 z1.part1.1changes

---
+++ Module1/Module1/FileSystemVisitor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+
+namespace Module1
+{
+    public class FileSystemVisitor
+    {
+        public delegate IEnumerable Algorithm(string path, string searchParameter);
+        public Algorithm _method;
+
+        public FileSystemVisitor(Algorithm method)
+        {
+            _method = method;
+        }
+
+        public static IEnumerable GetAllDirectionsAndFilesForSearchParameter(string path, string searchParameter)
+        {
+            foreach (var directory in Directory.EnumerateDirectories(path, searchParameter, SearchOption.AllDirectories))
+            {
+                yield return directory;
+                foreach (var files in Directory.EnumerateFiles(path, searchParameter, SearchOption.AllDirectories))
+                {
+                    yield return files;
+                }                
+            }
+        }        
+    }
+}
---
+++ Module1/Module1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Module1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Your path:");
+            string path = @"E:\Music";
+
+            string searchParameter = "*";
+
+            FileSystemVisitor visitor = new FileSystemVisitor(new FileSystemVisitor.Algorithm(FileSystemVisitor.GetAllDirectionsAndFilesForSearchParameter));
+            
+            foreach (var item in visitor._method.Invoke(path, searchParameter))
+            {
+                //Thread.Sleep(1000);
+                Console.WriteLine("{0}", item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
