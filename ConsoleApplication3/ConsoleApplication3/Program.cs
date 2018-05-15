446d27b1f2b608a9d3e044f5d815081e5d650772
TheCloudOftheUnicorn <aliaksandra.rybakovich@mail.ru>
2018-05-15 15:57:26 +0300

    Library for convert of string to int

---
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Module2._2;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string str = Console.ReadLine();
+
+                try
+                {
+                    Console.WriteLine("{0}", str.ConvertStringToInt());
+                }
+                catch (OverflowException e) { Console.WriteLine("Ошибка: " + e.Message); }
+                catch (ArgumentException e) { Console.WriteLine("Ошибка: " + e.Message); }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Module2.2/Module2.2/ConvertString.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Module2._2
+{
+    public static class ConvertString
+    {
+        public static int ConvertStringToInt(this string str)
+        {
+            int finalNumeric = 0;
+            if ((Regex.Match(str, @"(^(\d)(\d))|(^(\-\d)(\d))").Success) && (str != null))
+            {
+                int pow = str.Length - 1;
+                for (int i = pow; i >= 0; i--)
+                {
+                    char[] charElements = str.ToCharArray();
+                    if (charElements[i] != '-')
+                    {
+                        checked
+                        {
+                            try
+                            {
+                                finalNumeric += (int)Char.GetNumericValue(charElements[i]) * (int)Math.Pow(10, pow - i);
+                            }
+                            catch (OverflowException e)
+                            {
+                                throw new OverflowException("Число с переполнением: " + unchecked(finalNumeric));
+                            }
+                        }
+                    }
+                    else
+                    {
+                        finalNumeric *= (-1);
+                    }
+                }
+
+                return finalNumeric;
+            }
+            else { throw new ArgumentException("Введите число!"); }
+        }
+    }
+}
+
+
